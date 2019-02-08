8cde03456ad33872e0f65f9ff9cb8b4192bd383b
siddhant1 <sidhant.manchanda@gmail.com>
2019-02-08 13:26:11 +0530

    Added Tree BFS and .prettierrc

--- Data Structures/Trees/BInary_Search_Tree_Recursive.js
+++ Data Structures/Trees/BInary_Search_Tree_Recursive.js
@@ -1,69 +1,88 @@
 class Node {
   constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+    this.value = value
+    this.left = null
+    this.right = null
   }
 
   findNode(value) {
     if (this.value === value) {
-      return true;
+      return true
     } else {
       if (value < this.value) {
         if (this.left === null) {
-          return false;
-        } else return this.left.findNode(value);
+          return false
+        } else return this.left.findNode(value)
       } else if (value > this.value) {
         if (this.right === null) {
-          return false;
-        } else return this.right.findNode(value);
+          return false
+        } else return this.right.findNode(value)
       }
     }
   }
   addNode(value) {
     if (value < this.value) {
       if (this.left === null) {
-        this.left = new Node(value);
-        return;
+        this.left = new Node(value)
+        return
       } else {
-        this.left.addNode(value);
+        this.left.addNode(value)
       }
     } else if (value > this.value) {
       if (this.right === null) {
-        this.right = new Node(value);
-        return;
+        this.right = new Node(value)
+        return
       } else {
-        this.right.addNode(value);
+        this.right.addNode(value)
       }
     } else {
-      return;
+      return
     }
   }
 }
 
 class BinarySearchTree {
   constructor() {
-    this.root = null;
+    this.root = null
   }
   insert(value) {
     if (!this.root) {
-      this.root = new Node(value);
+      this.root = new Node(value)
     } else {
-      this.root.addNode(value);
+      this.root.addNode(value)
     }
   }
 
   find(value) {
-    return this.root.findNode(value);
+    return this.root.findNode(value)
+  }
+
+  bfs() {
+    // Use this method to traverse your tree using Breadth First Search Technique
+    const queue = []
+    const visited = []
+    queue.push(this.root)
+    while (queue.length) {
+      let element = queue.shift()
+      visited.push(element.value)
+      if (element.left) {
+        queue.push(element.left)
+      }
+      if (element.right) {
+        queue.push(element.right)
+      }
+    }
+    return visited
   }
 }
 
-const tree = new BinarySearchTree();
-tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
-console.log(tree.find(71));
+const tree = new BinarySearchTree()
+tree.insert(10)
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+console.log(tree.bfs())
+// tree.insert(7);
+console.log(tree.find(71))
